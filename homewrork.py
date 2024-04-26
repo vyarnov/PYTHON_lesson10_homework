bc4dcce9579a9de6611c357d89933d6d29504b8f
Vitaly Yarnov <vyarnov@gmail.com>
2024-04-26 10:12:57 +0300

    add some text to home work 10

---
+++ homewrork.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import random
+
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data["whoAmI"] == "robot"] = 1
+data.loc[data["whoAmI"] == "human"] = 0
+data.head()
+
+print(data.head())
